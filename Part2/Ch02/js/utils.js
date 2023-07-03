bbd66d5ebe4e7962ca25015bc51af634c3c66e0a
younhoso <thdbsgh3443@naver.com>
2023-07-04 06:37:23 +0900

    update: 불꽃놀이 스크립트 만들기

--- Part2/Ch02/js/utils.js
+++ Part2/Ch02/js/utils.js
@@ -1,3 +1,9 @@
+/**
+ * 랜덤한 값을 리턴
+ * @param {Number} min 최소값
+ * @param {Number} max 최대값
+ * @returns {Number} 최소와 최대 사이의 랜덤한 값을 리턴
+ */
 export const randomNumBetween = (min, max) => {
   return Math.random() * (max - min) + min
 }
